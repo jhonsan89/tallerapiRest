9def1cd7985fa80a66e1e7c0a3f24e0f17cd9adf
JHON EDISON SANDOVAL BARRETO <JHONSAN89@hotmail.com>
2018-10-14 15:41:38 -0500

    api publicaciones

---
+++ Tallerapi.api/Controllers/PublicacionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tallerapi.api.Models;
+
+namespace Tallerapi.api.Controllers
+{
+    public class PublicacionController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Publicacion> Get()
+        {
+            using (var context = new PublicacionContext())
+            {
+                return context.Publicaciones.ToList();
+            }
+        }
+    }
+}
